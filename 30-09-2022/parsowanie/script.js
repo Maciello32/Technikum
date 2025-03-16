65d1454ff42c713648ccf9945ed0d0c40e01202e
Maciello32 <maciello2278@gmail.com>
2025-03-16 23:26:17 +0100

    Create script.js

---
+++ 30-09-2022/parsowanie/script.js
@@ -0,0 +1,8 @@
+function suma()
+    {
+let a = document.getElementById("a").value;
+let b = document.getElementById("b").value;
+a = parseFloat(a);
+b = parseFloat(b);
+document.getElementById("wynik").innerHTML = a + b
+}
