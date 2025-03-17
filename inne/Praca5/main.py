f10d5e607db1a374e4e78e198e54d17687a6c67c
Maciello32 <maciello2278@gmail.com>
2025-03-17 02:41:43 +0100

    Create main.py

---
+++ inne/Praca5/main.py
@@ -0,0 +1,17 @@
+#Ćw 2.
+#netto = 25
+#VAT = 120/100
+#brutto = (netto * VAT)
+#print (brutto,"zł")
+
+#Ćw 3.
+#brutto = 165
+#vat = 123/100
+#netto = (brutto / vat)
+#print(round(netto, 2),"zł")
+
+#Ćw 4.
+#brutto = 215
+#vat = 119/100
+#netto = (brutto / vat)
+#print(round(netto, 2),"zł")
